ad6c355ff82761f0cbbe22fd1dcf827663124674
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-11-12 11:37:05 -0500

    testing

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,25 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+const server = require("./server");
+const request = require("supertest");
+const db = require("../data/dbConfig");
+
+test("sanity", () => {
+  expect(true).toBe(true);
+});
+
+test("is testing environment", () => {
+  expect(process.env.NODE_ENV).toBe("testing");
+});
+
+describe("[POST] to /api/auth/register", () => {
+  test.todo("res.status, 201");
+  test.todo("returns proper body");
+  test.todo("fails if user already exists");
+  test.todo("username and password required");
+});
+
+describe("[POST] to /api/auth/login", () => {
+  test.todo("res.status, 200");
+  test.todo("success, provides message and token");
+  test.todo("fail message for user not existing");
+  test.todo("username and password required");
+});
